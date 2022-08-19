d07e20be38d1711cd0c2f14507ae356159b9538f
Trevor Matheson <trevor.matheson@netdocuments.com>
2022-08-19 13:54:35 -0600

    Added calculator methods for addition, subtraction, multiplication, division, and mod.

--- ruby/basics/homeworks/simple_calculator.rb
+++ ruby/basics/homeworks/simple_calculator.rb
@@ -4,4 +4,20 @@ puts "\nEnter the first number."
 first_number = gets.chomp
 puts "Enter the second number."
 second_number = gets.chomp
+
+#Evaluate addition.
+puts "#{first_number} + #{second_number} = #{first_number.to_i + second_number.to_i}"
+
+
+#Evaluate subtraction.
+puts "#{first_number} - #{second_number} = #{first_number.to_i - second_number.to_i}"
+
+
+#Evaluate multiplication.
 puts "#{first_number} * #{second_number} = #{first_number.to_i * second_number.to_i}"
+
+#Evaluate division.
+puts "#{first_number} / #{second_number} = #{first_number.to_f / second_number.to_f}"
+
+#Evaluate mod.
+puts "#{first_number} % #{second_number} = #{first_number.to_i % second_number.to_i}"
