dd3bdcda0e41978223e0d8dd4fc2d8f9b8e39474
Trevor Matheson <trevor.matheson@netdocuments.com>
2022-08-19 13:23:00 -0600

    Finished notes for strings pt 2. Completed homework analyzer project.

--- ruby/basics/working_with_strings_pt_2.rb
+++ ruby/basics/working_with_strings_pt_2.rb
@@ -3,3 +3,8 @@ puts "What is your first name?"
 first_name = gets.chomp
 puts "Thank you, #{first_name}!"
 
+# gets.chomp method returns a string
+# * will concatonate a string
+puts "\nPlease enter your age."
+age = gets.chomp.to_i
+puts "Two times your age is #{age * 2}."
