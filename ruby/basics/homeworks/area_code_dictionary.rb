1ce57c835ab7ea3f0fe1ab7d943abdf1da442eb2
Trevor Matheson <trevor.matheson@netdocuments.com>
2022-08-29 15:20:31 -0600

    Completed area_code_dictionary homework.

--- ruby/basics/homeworks/area_code_dictionary.rb
+++ ruby/basics/homeworks/area_code_dictionary.rb
@@ -1,27 +1,56 @@
 dial_book = {
-  "newyork" => "212",
-  "newbrunswick" => "732",
-  "edison" => "908",
-  "plainsboro" => "609",
-  "sanfrancisco" => "301",
-  "miami" => "305",
-  "paloalto" => "650",
-  "evanston" => "847",
-  "orlando" => "407",
-  "lancaster" => "717"
+  "New York" => "212",
+  "New Brunswick" => "732",
+  "Edison" => "908",
+  "Plainsboro" => "609",
+  "San Francisco" => "301",
+  "Miami" => "305",
+  "Paloalto" => "650",
+  "Evanston" => "847",
+  "Orlando" => "407",
+  "Lancaster" => "717"
 }
  
 # Get city names from the hash
 def get_city_names(somehash)
-# Write code here
+  somehash.each { |k, v| puts k }
 end
  
 # Get area code based on given hash and key
 def get_area_code(somehash, key)
-# Write code here
+  area_code_found = false
+
+  somehash.each do |k, v|
+    if k.downcase == key
+      puts "The area code for #{k} is #{v}"
+      area_code_found = true
+      break
+    end
+  end
+
+  if area_code_found == false
+    puts "Could not validate area code for #{key}. Please try again."
+  end
 end
  
 # Execution flow
 loop do
-# Write your program execution code here
+  print "- Do you want to look up an area code based on a city name?(Y/N): "
+  user_response = gets.chomp.upcase
+
+  if user_response == "Y"
+    puts "- Which city do you want the area code for?"
+
+    get_city_names(dial_book)
+
+    print "- Enter your selection: "
+
+    user_response = gets.chomp.downcase
+    get_area_code(dial_book, user_response)
+
+    elsif user_response == "N"
+    break
+  else
+    puts "That is not a valid entry."
+  end
 end
