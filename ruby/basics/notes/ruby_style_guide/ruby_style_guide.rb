d1bca8804d80cc29e887a26e2f7cd957324511f4
Trevor Matheson <trevor.matheson@netdocuments.com>
2022-08-29 14:37:21 -0600

    Added notes for style guidelines.

--- ruby/basics/notes/ruby_style_guide/ruby_style_guide.rb
+++ ruby/basics/notes/ruby_style_guide/ruby_style_guide.rb
@@ -1,4 +1,4 @@
-# Ruby and ruby on rails its best practice to use snake case for method calls and variable assignments.
+# In Ruby and ruby on rails it is best practice to use snake case for method calls and variable assignments.
 this_is_snake_case
 
 also_snake_case
@@ -6,4 +6,9 @@ also_snake_case
 # Indentation in ruby and ruby on rails is 2 spaces for tab.
 def my_name_is
   # Here is where my code lies
+end
+
+# Class names should be camel case.
+class SomeClass
+
 end
