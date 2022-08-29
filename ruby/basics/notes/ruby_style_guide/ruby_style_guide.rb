b106b8100db1071c9a22a3ead757e5bbdd3e963e
Trevor Matheson <trevor.matheson@netdocuments.com>
2022-08-29 14:30:00 -0600

    Fixed some spacing issues.

---
+++ ruby/basics/notes/ruby_style_guide/ruby_style_guide.rb
@@ -0,0 +1,9 @@
+# Ruby and ruby on rails its best practice to use snake case for method calls and variable assignments.
+this_is_snake_case
+
+also_snake_case
+
+# Indentation in ruby and ruby on rails is 2 spaces for tab.
+def my_name_is
+  # Here is where my code lies
+end
