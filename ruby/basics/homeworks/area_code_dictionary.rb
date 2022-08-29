f40d0f785f6f5a28e1b5ab0431fc115a9ed9fba9
Trevor Matheson <trevor.matheson@netdocuments.com>
2022-08-29 15:34:33 -0600

    Made changes since methods were doing more than they were meant to.

--- ruby/basics/homeworks/area_code_dictionary.rb
+++ ruby/basics/homeworks/area_code_dictionary.rb
@@ -1,14 +1,14 @@
 dial_book = {
-  "New York" => "212",
-  "New Brunswick" => "732",
-  "Edison" => "908",
-  "Plainsboro" => "609",
-  "San Francisco" => "301",
-  "Miami" => "305",
-  "Paloalto" => "650",
-  "Evanston" => "847",
-  "Orlando" => "407",
-  "Lancaster" => "717"
+  "new york" => "212",
+  "new brunswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "san francisco" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717"
 }
  
 # Get city names from the hash
@@ -18,19 +18,7 @@ end
  
 # Get area code based on given hash and key
 def get_area_code(somehash, key)
-  area_code_found = false
-
-  somehash.each do |k, v|
-    if k.downcase == key
-      puts "The area code for #{k} is #{v}"
-      area_code_found = true
-      break
-    end
-  end
-
-  if area_code_found == false
-    puts "Could not validate area code for #{key}. Please try again."
-  end
+  somehash[key]
 end
  
 # Execution flow
@@ -38,19 +26,14 @@ loop do
   print "- Do you want to look up an area code based on a city name?(Y/N): "
   user_response = gets.chomp.upcase
 
-  if user_response == "Y"
-    puts "- Which city do you want the area code for?"
-
-    get_city_names(dial_book)
-
-    print "- Enter your selection: "
-
-    user_response = gets.chomp.downcase
-    get_area_code(dial_book, user_response)
-
-    elsif user_response == "N"
-    break
+  break if user_response != "Y"
+  puts "- Which city do you want the area code for?"
+  get_city_names(dial_book)
+  print "- Enter your selection: "
+  user_response = gets.chomp.downcase
+  if dial_book.include?(user_response)
+    puts "The area code for #{user_response} is #{get_area_code(dial_book, user_response)}."
   else
-    puts "That is not a valid entry."
+    puts "You entered a city name not in the dictionary"
   end
 end
