142be9b52b2902b047613e4ca07fecbbb5a0d5be
Trevor Earl Matheson <trevor.earl.matheson@gmail.com>
2022-09-13 22:44:15 -0600

    Added notes for initializing objects using getters and setters and then also for creating new objects and interacting with them, say changing attributes.

--- ruby_learning/oop/intro_to_oop/student.rb
+++ ruby_learning/oop/intro_to_oop/student.rb
@@ -1,14 +1,21 @@
 class Student
-    @first_name
-    @last_name
-    @email
-    @username
-    @password
+  attr_accessor :first_name, :last_name, :email, :username, :password
 
-    def to_s
-        "First name: #{@first_name}"
-    end
-    
+  def initialize(first_name, last_name, username, email, password)
+    @first_name = first_name
+    @last_name = last_name
+    @username = username
+    @email = email
+    @password = password
+  end
+
+  def to_s
+    "First name: #{@first_name}, Last name: #{last_name}, Username: #{username}, Email: #{email}, Password: #{password}"
+  end
 end
 
-trevor = Student.new
+trevor = Student.new("Trevor", "Matheson", "trevor123", "trevor.earl.matheson@gmail.com", "password1")
+john = Student.new("John", "Doe", "john123", "john.doe@gmail.com", "password2")
+
+puts trevor
+puts john
